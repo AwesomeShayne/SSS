2acba8a15ff41706a23544176e5d116568199d38
Shayne <Shayne.Hemminger@louisville.edu>
2016-10-04 14:01:09 -0400

    Various Changes to add a GUI to Open to, as well as a method to change between different sets of Content

---
+++ SSS/LaunchScreen.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SSS
+{
+    /// <summary>
+    /// Interaction logic for LaunchScreen.xaml
+    /// </summary>
+    public partial class LaunchScreen : UserControl
+    {
+        private MainWindow Parent;
+        public LaunchScreen(MainWindow _Parent)
+        {
+            InitializeComponent();
+            Parent = _Parent;
+            SwitchContentButton.Click += SwitchContentButton_Click;
+        }
+
+        private void SwitchContentButton_Click(object sender, RoutedEventArgs e)
+        {
+            Parent.SetContent(new Visualizer());
+        }
+    }
+}
